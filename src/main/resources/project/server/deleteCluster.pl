5df8728fc0860d9868552aae658a59f318466459
Vinayak Shinde <vinayak.shinde@clogeny.com>
2015-03-17 13:28:58 +0530

    Implemented code review comments. 1. Removed Delete Forcefully flag and cell name parameter.2. Removed the jython code to stop the cluster before deleteing it since AdminClusterManagement.deleteCluster() is capable of deleting running cluster. 3. Changed documentation for wsadmin path. 4. Changed label for cluster Name to Cluster Name. 5. Renamed connectiontype to connectionType. 6. Changed documentation for connectionType.

--- src/main/resources/project/server/deleteCluster.pl
+++ src/main/resources/project/server/deleteCluster.pl
@@ -1,7 +1,7 @@
 
 =head1 NAME
 
-deleteJDBCProvider.pl - a perl library to delete cluster
+deleteCluster.pl - a perl library to delete cluster
 
 =head1 SYNOPSIS
 
@@ -43,12 +43,9 @@ $ec->abortOnError(0);
 
 $::gWSAdminAbsPath =
   ( $ec->getProperty("wsadminabspath") )->findvalue("//value");
-$::gCellName    = ( $ec->getProperty("cellName") )->findvalue("//value");
 $::gClusterName = ( $ec->getProperty("clusterName") )->findvalue("//value");
-$::gDeleteForcefully =
-  ( $ec->getProperty("deleteForcefully") )->findvalue("//value");
 $::gConnectionType =
-  ( $ec->getProperty("connectiontype") )->findvalue("//value");
+  ( $ec->getProperty("connectionType") )->findvalue("//value");
 $::gConfigurationName =
   ( $ec->getProperty("configname") )->findvalue("//value");
 
@@ -58,7 +55,7 @@ $::gConfigurationName =
 
 =over
 
-=item B<myCmdr>
+=item B<main>
 
 main - contains the whole process to be done by the plugin, it builds
        the command line, sets the properties and the working directory
@@ -85,51 +82,21 @@ sub main() {
     if ( $::gConfigurationName ne '' ) {
         %configuration = getConfiguration( $ec, $::gConfigurationName );
     }
+    else {
+        die "Error : configuration not provided.";
+    }
 
     push( @args, '"' . $::gWSAdminAbsPath . '"' );
 
     my $ScriptFile = "import sys\n";
 
-    if ($::gDeleteForcefully) {
-
-        ## Validate the cell name
-        $ScriptFile .= "cellname = AdminControl.getCell()\n";
-        $ScriptFile .= "if cellname != \"" . $::gCellName . "\":\n";
-        $ScriptFile .=
-          "\tprint 'Error : Cell " . $::gCellName . " does not exist.'\n";
-        $ScriptFile .= "\tsys.exit(1)\n";
-
-        ## Validate cluster name
-        $ScriptFile .= "result = AdminClusterManagement.checkIfClusterExists(\""
-          . $::gClusterName . "\")\n";
-        $ScriptFile .= "if result == \"false\":\n";
-        $ScriptFile .=
-          "\tprint 'Error : Cluster " . $::gClusterName . " does not exist.'\n";
-        $ScriptFile .= "\tsys.exit(1)\n";
-
-        $ScriptFile .= "\n"
-          . 'cluster = AdminControl.completeObjectName(\'cell='
-          . $::gCellName
-          . ',type=Cluster,name='
-          . $::gClusterName . ',*\')' . "\n";
-        $ScriptFile .= 'print cluster' . "\n";
-        $ScriptFile .= 'print \'Stopping cluster \'' . "\n";
-        $ScriptFile .= 'AdminControl.invoke(cluster, \'stop\')' . "\n";
-        $ScriptFile .=
-          'status = AdminControl.getAttribute(cluster, \'state\')' . "\n";
-        $ScriptFile .= 'desiredStatus = \'websphere.cluster.stopped\'' . "\n";
-        $ScriptFile .= 'print \'Cluster status = \' + status' . "\n";
-        $ScriptFile .= 'while 1:' . "\n";
-        $ScriptFile .= "\t"
-          . 'status = AdminControl.getAttribute(cluster, \'state\')' . "\n";
-        $ScriptFile .= "\t" . 'print \'Cluster status = \' + status' . "\n";
-        $ScriptFile .= "\t" . 'if status==desiredStatus:' . "\n";
-        $ScriptFile .= "\t\t" . 'break' . "\n";
-        $ScriptFile .= "\t" . 'else:' . "\n";
-        $ScriptFile .= "\t\t" . 'sleep(3)' . "\n";
-        $ScriptFile .= 'print \'Cluster stopped successfully\'' . "\n";
-
-    }
+    ## Validate cluster name
+    $ScriptFile .= "result = AdminClusterManagement.checkIfClusterExists(\""
+      . $::gClusterName . "\")\n";
+    $ScriptFile .= "if result == \"false\":\n";
+    $ScriptFile .=
+      "\tprint 'Error : Cluster " . $::gClusterName . " does not exist.'\n";
+    $ScriptFile .= "\tsys.exit(1)\n";
 
     $ScriptFile .= 'result = AdminClusterManagement.deleteCluster("'
       . $::gClusterName . '")' . "\n";
@@ -144,24 +111,20 @@ sub main() {
     push( @args, '-lang ' . DEFAULT_WSADMIN_LANGUAGE );
     push( @args, '-conntype ' . $::gConnectionType );
 
-    #inject config...
-    if (%configuration) {
+    if ( $configuration{'websphere_url'} ne '' ) {
+        push( @args, '-host ' . $configuration{'websphere_url'} );
+    }
 
-        if ( $configuration{'websphere_url'} ne '' ) {
-            push( @args, '-host ' . $configuration{'websphere_url'} );
-        }
+    if ( $configuration{'websphere_port'} ne '' ) {
+        push( @args, '-port ' . $configuration{'websphere_port'} );
+    }
 
-        if ( $configuration{'websphere_port'} ne '' ) {
-            push( @args, '-port ' . $configuration{'websphere_port'} );
-        }
+    if ( $configuration{'user'} ne '' ) {
+        push( @args, '-user ' . $configuration{'user'} );
+    }
 
-        if ( $configuration{'user'} ne '' ) {
-            push( @args, '-user ' . $configuration{'user'} );
-        }
-
-        if ( $configuration{'password'} ne '' ) {
-            push( @args, '-password ' . $configuration{'password'} );
-        }
+    if ( $configuration{'password'} ne '' ) {
+        push( @args, '-password ' . $configuration{'password'} );
     }
 
     my $cmdLine = createCommandLine( \@args );
