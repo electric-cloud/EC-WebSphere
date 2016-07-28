a879179e4f045ef192048f88a8b56f02807d337f
Vitaliy Batichko <vbatichko@gmail.com>
2016-07-27 19:59:05 -0700

    Fixes for ECPAPPSERVERWEBSPHERE-57 & ECPAPPSERVERWEBSPHERE-58
    * Updated screenshots for help page
    * Fixed splitting of multiline return values in python code

--- src/main/resources/project/wsadmin_scripts/discover.py
+++ src/main/resources/project/wsadmin_scripts/discover.py
@@ -1,5 +1,5 @@
 # Get list of Cells
-cells = AdminConfig.list('Cell').split()
+cells = AdminConfig.list('Cell').splitlines()
 
 for cell in cells:
  cname = AdminConfig.showAttribute(cell, 'name')
@@ -8,28 +8,28 @@ for cell in cells:
  server_entries = []
 
  # Get list of Nodes
- nodes = AdminConfig.list('Node', cell).split()
+ nodes = AdminConfig.list('Node', cell).splitlines()
  for node in nodes:
   nname = AdminConfig.showAttribute(node, 'name')
   print 'Node: ' + nname
 
   # Get list of servers on node. We only need servers of type APPLICATION
-  servers = AdminTask.listServers('[-serverType APPLICATION_SERVER -nodeName %s]' % nname).split()
+  servers = AdminTask.listServers('[-serverType APPLICATION_SERVER -nodeName %s]' % nname).splitlines()
 
   for server in servers:
    server_name = AdminConfig.showAttribute(server, "name")
-   server_apps = AdminApp.list("WebSphere:cell=%s,node=%s,server=%s" % (cname, nname, server_name)).split()
+   server_apps = AdminApp.list("WebSphere:cell=%s,node=%s,server=%s" % (cname, nname, server_name)).splitlines()
 
    print '\tServer: ' + server_name
    print '\t\t', server_apps
    server_entries += ['"%s=%s": [ %s ]' % (nname, server_name, ','.join([ '"%s"' % app for app in server_apps]))]
 
-  clusters = AdminConfig.list('ServerCluster').split()
+  clusters = AdminConfig.list('ServerCluster').splitlines()
 
   for cluster in clusters:
    cluster_name = AdminConfig.showAttribute(cluster, "name")
    print '\tCluster: ' + cluster_name
-   cluster_apps = AdminApp.list("WebSphere:cell=%s,node=%s,cluster=%s" % (cname, nname, cluster_name)).split()
+   cluster_apps = AdminApp.list("WebSphere:cell=%s,node=%s,cluster=%s" % (cname, nname, cluster_name)).splitlines()
    print '\t\t', cluster_apps
    cluster_entries += ['"%s": [ %s ]' % (cluster_name, ','.join([ '"%s"' % app for app in cluster_apps]))]
 
