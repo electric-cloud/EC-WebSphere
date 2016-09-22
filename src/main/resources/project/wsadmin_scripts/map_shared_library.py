cf99efb864b1dcda37e3e9660ac97d4ec85b48da
justnoxx <justnoxx@gmail.com>
2016-09-22 15:17:58 +0300

    Added checks for steps

--- src/main/resources/project/wsadmin_scripts/map_shared_library.py
+++ src/main/resources/project/wsadmin_scripts/map_shared_library.py
@@ -1,14 +1,37 @@
+import sys
+
 libraryName = '$[libraryName]'
 applicationName = '$[applicationName]'
 
+# library section
 library = AdminConfig.getid('/Library:%s/' % (libraryName))
-print library
+if not library :
+    print "No library was found by name provided"
+    sys.exit(1);
+print "Library: " + library
+
+# application section
 deployment = AdminConfig.getid('/Deployment:%s/' % (applicationName))
-print deployment
+if not deployment :
+    print "No application was found by name provided"
+    sys.exit(1);
+print "Depoyment: " + deployment
+
+# deployment section
 appDeploy = AdminConfig.showAttribute(deployment, 'deployedObject')
-print appDeploy
+if not appDeploy:
+    print "DeployedObject was not found"
+    sys.exit(1);
+print "AppDeploy: " + appDeploy
+
+# classloader
 classLoad1 = AdminConfig.showAttribute(appDeploy, 'classloader')
-print classLoad1
-print AdminConfig.create('LibraryRef', classLoad1, [['libraryName', 'javalib']])
+if not classLoad1:
+    print "ClassLoader was not found"
+    sys.exit(1);
+print "ClassLoader: " + classLoad1
+
+# result
+print AdminConfig.create('LibraryRef', classLoad1, [['libraryName', libraryName]])
 AdminConfig.save()
 
