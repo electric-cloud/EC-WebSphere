4fe185aab2461f43797ee22712abaf8c646451a2
Polina Shubina <925043@gmail.com>
2016-10-06 11:50:15 +0000

    Added quotes to path

--- src/main/resources/project/wsadmin_scripts/update_app.py
+++ src/main/resources/project/wsadmin_scripts/update_app.py
@@ -18,7 +18,7 @@ $[serverName]
 
 
 updateCommand = r'''
-[-operation $[operation] -contents $[content]
+[-operation $[operation] -contents "$[content]"
 '''.strip()
 if contentURI:
     updateCommand + ' ' # just whitespace - as a delimeter
