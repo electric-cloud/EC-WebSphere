cfee0191e74b729a289a982fd71c9d514b14d147
Dmitriy Shamatrin <justnoxx@gmail.com>
2018-02-15 15:08:38 +0200

    Added source files

---
+++ specs/environments/v85nd/setEnv.sh
@@ -0,0 +1,7 @@
+export WAS_USERNAME="wsadmin"
+export WAS_PASSWORD="changeme";
+export WAS_HOST="websphere85nd";
+export WAS_PORT="8879";
+export WAS_CONNTYPE="SOAP";
+export WAS_DEBUG="0";
+export WSADMIN_PATH="/opt/IBM/WebSphere/AppServer/profiles/Dmgr01/bin/wsadmin.sh";
