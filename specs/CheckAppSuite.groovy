72b51b1491a9894c32db21044f2895b16842b051
PVOVK\Pavel Vovk <wolf.od@gmail.com>
2018-02-23 18:20:21 +0200

    Update error messages

--- specs/CheckAppSuite.groovy
+++ specs/CheckAppSuite.groovy
@@ -6,6 +6,52 @@ import PluginTestHelper
 @Stepwise
 class CheckApp extends PluginTestHelper {
     @Shared
+    def expectedOutcomes = [
+        success: 'success',
+        error: 'error',
+        warning: 'warning',
+        running: 'running'
+    ]
+    
+    @Shared
+    def expectedUpperStepSummaries = [
+        fieldRequired: '',
+        incorrectComfigname: "Configuration 's' doesn't exists",
+        incorectState: "is not valid application state to be checked. Expected on of: EXISTS, NOT_EXISTS, READY, NOT_READY, RUNNING, NOT_RUNNING",
+        incorrectAppName: "is not installed, expected",
+        appIsNotInstalled_IsInstalled: "is not installed, expected: application is installed",
+        appIsNotInstalled_IsNotReady: "is not installed, expected: application is not ready",
+        appIsNotInstalled_IsReady: "is not installed, expected: application is ready",
+        appIsNotInstalled_IsNotRunning: "is not installed, expected: application is not runnning",
+        appIsNotInstalled_IsRunning: "is not installed, expected: application is runnning",
+        appIsInstalled_IsNotInstalled: "is installed, expected: application is not installed",
+        appIsInstalled_IsNotReady: "is installed, expected: application is not ready",
+        appIsInstalled_IsReady: "is installed, expected: application is ready",
+        appIsInstalled_IsNotRunning: "is installed, expected: application is not runnning",
+        appIsInstalled_IsRunning: "is installed, expected: application is runnning",
+        appIsNotReady_IsNotInstalled: "is not ready, expected: application is not installed",
+        //appIsNotReady_IsInstalled: "",
+        appIsNotReady_IsReady: "is not ready, expected: application is ready",
+        appIsNotReady_IsNotRunning: "is not ready, expected: application is not runnning",
+        appIsNotReady_IsRunning: "is not ready, expected: application is runnning",
+        appIsReady_IsNotInstalled: "is not ready, expected: application is not installed",
+        //appIsReady_IsInstalled: "",
+        appIsReady_IsNotReady: "is ready, expected: application is not ready",
+        appIsReady_IsNotRunning: "is ready, expected: application is not runnning",
+        appIsReady_IsRunning: "is ready, expected: application is runnning",
+        appIsNotRunning_IsNotInstalled: "is not running, expected: application application is not installed",
+        //appIsNotRunning_IsInstalled: "",
+        appIsNotRunning_IsNotReady: "is not running, expected: application is not ready",
+        //appIsNotRunning_IsReady: "",
+        appIsNotRunning_IsRunning: "is not running, expected: application is runnning",
+        appIsRunning_IsNotInstalled: "is running, expected: application application is not installed",
+        //appIsRunning_IsInstalled: "",
+        appIsRunning_IsNotReady: "is running, expected: application is not ready",
+        //appIsRunning_IsReady: "",
+        appIsRunning_IsNotRunning: "is running, expected: application is not runnning",
+    ]
+
+    @Shared
     def testProjectName = 'EC-WebSphere-Specs-CheckApp'
     @Shared
     def testProcedureName = 'CheckApp'
@@ -251,7 +297,7 @@ class CheckApp extends PluginTestHelper {
         def debugLog = getJobLogs(result.jobId)
         println "Procedure log:\n$debugLog\n"
 
-        assert outcome == expectedOutcome
+        assert outcome.contains(expectedOutcome)
 
         where:
         wsConfigName            | wsAdminAbsolutePath               | wsApplicationName                         | wsApplicationState                | tTime      | expectedOutcome
