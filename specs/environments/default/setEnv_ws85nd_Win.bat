d8288feb4e6dc46cf364c5be782e8d53038bec0d
PVOVK\Pavel Vovk <wolf.od@gmail.com>
2018-03-01 12:12:23 +0200

    Change the Vaule for debug from 0 to 1

--- specs/environments/default/setEnv_ws85nd_Win.bat
+++ specs/environments/default/setEnv_ws85nd_Win.bat
@@ -2,5 +2,6 @@ set WAS_USERNAME=wsadmin
 set WAS_PASSWORD=changeme
 set WAS_HOST=websphere85nd
 set WAS_PORT=8879
+set WAS_DEBUG=1
 set WSADMIN_PATH=/opt/IBM/WebSphere/AppServer/profiles/Dmgr01/bin/wsadmin.sh
 set WAS_APPPATH=/var/tmp/
--- specs/environments/v85s/setEnv_Win.bat
+++ specs/environments/v85s/setEnv_Win.bat
@@ -3,6 +3,6 @@ set WAS_PASSWORD=changeme
 set WAS_HOST=websphere85s
 set WAS_PORT=8879
 set WAS_CONNTYPE=SOAP
-set WAS_DEBUG=0
+set WAS_DEBUG=1
 set WSADMIN_PATH=/opt/IBM/WebSphere/AppServer/profiles/Dmgr01/bin/wsadmin.sh
 set WAS_APPPATH =C:\
