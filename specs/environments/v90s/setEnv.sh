f5d19b386ee3f25385ccfc95e561c5395f3b9990
PVOVK\Pavel Vovk <wolf.od@gmail.com>
2018-03-03 15:28:02 +0200

    Update Stande Alone Config for

--- specs/environments/v90s/setEnv.sh
+++ specs/environments/v90s/setEnv.sh
@@ -4,5 +4,5 @@ export WAS_HOST="websphere90s";
 export WAS_PORT="8880";
 export WAS_CONNTYPE="SOAP";
 export WAS_DEBUG="1";
-export WSADMIN_PATH="/opt/IBM/WebSphere/AppServer/profiles/Dmgr01/bin/wsadmin.sh";
+export WSADMIN_PATH="/opt/IBM/WebSphere/AppServer/profiles/AppSrv01/bin/wsadmin.sh";
 export WAS_APPPATH="/var/tmp/";
