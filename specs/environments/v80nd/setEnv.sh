552a6fc079e696e9e510440363effc5d798ce214
PVOVK\Pavel Vovk <wolf.od@gmail.com>
2018-03-07 15:28:19 +0200

    Add configurations 80 nd

---
+++ specs/environments/v80nd/setEnv.sh
@@ -0,0 +1,8 @@
+export WAS_USERNAME="wsadmin"
+export WAS_PASSWORD="changeme";
+export WAS_HOST="websphere80nd";
+export WAS_PORT="8879";
+export WAS_CONNTYPE="SOAP";
+export WAS_DEBUG="1";
+export WSADMIN_PATH="/opt/IBM/WebSphere/AppServer/profiles/Dmgr01/bin/wsadmin.sh";
+export WAS_APPPATH ="/var/tmp/";
