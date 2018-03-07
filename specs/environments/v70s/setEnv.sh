64c5d61a92279d4784e1e655aeccc11535938844
PVOVK\Pavel Vovk <wolf.od@gmail.com>
2018-03-07 14:34:36 +0200

    Added configuration v7.0s

---
+++ specs/environments/v70s/setEnv.sh
@@ -0,0 +1,9 @@
+export WAS_USERNAME="wsadmin"
+export WAS_PASSWORD="changeme";
+export WAS_HOST="websphere70s";
+export WAS_PORT="8880";
+export WAS_CONNTYPE="SOAP";
+export WAS_DEBUG="1";
+export WSADMIN_PATH="/opt/IBM/WebSphere/AppServer/profiles/AppSrv01/bin/wsadmin.sh";
+export WAS_APPPATH="/var/tmp/";
+
