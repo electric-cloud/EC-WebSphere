b59e1a22a624629e9daa04757e908a0fe5b758e2
Dmitriy Shamatrin <justnoxx@gmail.com>
2018-03-08 09:20:32 +0200

    Added v85s and v90s envs for websphere

---
+++ specs/environments/v90s_win/waitForEnv.pl
@@ -0,0 +1,25 @@
+use strict;
+use warnings;
+use LWP::UserAgent;
+
+$| = 1;
+
+$ENV{PERL_LWP_SSL_VERIFY_HOSTNAME} = 0;
+my $url = 'https://websphere90s:9443/ibm/console';
+my $ua = LWP::UserAgent->new();
+
+my $done = 0;
+print "will wait until app server is up\n";
+while (!$done) {
+    my $resp = $ua->get($url);
+    my $code = $resp->code();
+    print $resp->decoded_content();
+    print "Code: $code\n";
+    if ($resp->code() eq '404') {
+        $done = 1;
+        last;
+    }
+    sleep 10;
+}
+
+exit 0;
