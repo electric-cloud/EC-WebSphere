a2c138c62d3f7dd779189bc5d4aef7aa7b2d875d
Dmitriy Shamatrin <justnoxx@gmail.com>
2018-08-09 17:49:48 +0300

    New websphere exception handling

--- src/main/resources/project/server/Server/deleteApplicationServer.pl
+++ src/main/resources/project/server/Server/deleteApplicationServer.pl
@@ -14,116 +14,53 @@
 #  limitations under the License.
 #
 
-
-# -------------------------------------------------------------------------
-# Includes
-# -------------------------------------------------------------------------
+use strict;
+use warnings;
 use ElectricCommander;
 use ElectricCommander::PropMod qw(/myProject/modules);
 use WebSphere::WebSphere;
 use WebSphere::Util;
+use Data::Dumper;
 
-use warnings;
-use strict;
-$| = 1;
-
-# -------------------------------------------------------------------------
-# Variables
-# -------------------------------------------------------------------------
-
-my $config = '$[configname]';
-my $node_name = '$[wasNodeName]';
-my $app_server_name = '$[wasAppServerName]';
-
-rtrim(
-    $config,
-    $node_name,
-    $app_server_name
-);
 
 my $ec = ElectricCommander->new();
 $ec->abortOnError(0);
 
-my $websphere = WebSphere::WebSphere->new(
-    $ec,
-    $config,
-);
-
-$websphere->setTemplateProperties(
-    nodeName  => $node_name,
-    appServerName => $app_server_name
-);
-
-my $logger = $websphere->log();
-my $file = 'delete_application_server.py';
-my $script = $ec->getProperty("/myProject/wsadmin_scripts/$file")->getNodeText('//value');
-
-
-
-$file = $websphere->write_jython_script(
-    $file, {},
-    augment_filename_with_random_numbers => 1
-);
-
-
-my $shellcmd = $websphere->_create_runfile($file, ());
-my $escaped_shellcmd = $websphere->_mask_password($shellcmd);
-
-$logger->info('WSAdmin command line: ', $escaped_shellcmd);
-
-$logger->debug("WSAdmin script:");
-$logger->debug($script);
-$logger->debug("== End of WSAdmin script ==");
-
-my %props = ();
-
-$props{deleteApplicationServerLine} = $escaped_shellcmd;
-
-# execute
-my $cmd_res = `$shellcmd 2>&1`;
-
-$logger->info($cmd_res);
-
-my $code = $?;
-$code >> 8;
-
-my $result_params = {
-    outcome => {
-        target => 'myCall',
-        result => '',
-    },
-    procedure => {
-        target => 'myCall',
-        msg => ''
-    },
-    pipeline => {
-        target => 'Delete Application Server Result:',
-        msg => '',
-    }
+my $opts = {
+    disabled_wsadmin_check => 0
 };
 
-$logger->info("Exit code: $code\n");
-if ($code == SUCCESS) {
-    $result_params->{outcome}->{result} = 'success';
-    $result_params->{procedure}->{msg} = sprintf(
-        'Application Server %s has been deleted on node %s.',
-        $app_server_name, $node_name
-    );
-    $result_params->{pipeline}->{msg} = $result_params->{procedure}->{msg};
-}
-else {
-    my $error = $websphere->extractWebSphereExceptions($cmd_res);
-    $result_params->{outcome}->{result} = 'error';
-    $result_params->{procedure}->{msg} = $result_params->{pipeline}->{msg} =
-        sprintf(
-            'Failed to delete Application Server %s on node %s.' . "\n" . 'Error: %s',
-            $app_server_name,
-            $node_name,
-            $error
-        );
-}
+my $websphere = WebSphere::WebSphere->new($ec, '', '', $opts);
 
-my $exit = $websphere->setResult(%$result_params);
+$websphere->{jobStepId} = '$[jobStepId]';
 
-$exit->();
 
+my $step_params = {
+    target => {
+        pipeline => 'Delete Application Server Result:',
+        success_summary => 'Application Server has been deleted.',
+        error_summary   => 'Failed to delete an Application Server.'
+    },
+    # error_cb => sub {
+    #     my ($self) = @_;
+    #     my $errors = $self->extractWebSphereExceptions($self->cmd_res);
+    #     if ($errors) {
+    #         $self->procedure_result()->{procedure}->{msg} .= ":\n$errors";
+    #         $self->procedure_result()->{pipeline}->{msg} .= ":\n$errors";
+    #     }
+    # },
+    jython_script => {
+        path => 'delete_application_server.py',
+    },
+    # args_cb => \&WebSphere::WebSphere::args_cb_start_node,
+    # procedure_result_cb => \&WebSphere::WebSphere::procedure_result_cb_start_node
+};
+
+eval {
+    $websphere->run_step($websphere->config_values()->{wsadminabspath}, $step_params);
+    1;
+} or do {
+    my $exception = $@;
+    rtrim($exception);
+    $websphere->bail_out($exception);
+};
