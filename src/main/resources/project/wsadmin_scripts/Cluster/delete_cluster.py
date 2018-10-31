6107602348b2bb0868d30947509e4a4d936e6bfa
Dmitriy Shamatrin <justnoxx@gmail.com>
2018-10-31 15:00:50 +0200

    Added logic of cluster state validation befor deleting to avoid undefined behaviours

--- src/main/resources/project/wsadmin_scripts/Cluster/delete_cluster.py
+++ src/main/resources/project/wsadmin_scripts/Cluster/delete_cluster.py
@@ -7,6 +7,19 @@ $[wasClusterName]
 syncNodes = '''
 $[wasSyncNodes]
 '''.strip()
+
+if not isClusterExists(clusterName):
+    bailOut("Cluster %s does not exist" % (clusterName))
+
+# Using websphere UI we can't delete running cluster. But using jython it is possible.
+# During testing we've found that it leads to undefined behaviour and may damage websphere instance.
+# So, we will abort this job if cluster is not stopped.
+clusterState = getClusterState(clusterName)
+if clusterState != 'websphere.cluster.stopped':
+    bailOut("Cluster %s can't be deleted because it is running.\n\
+Deletion of running cluster may damage websphere instance and leads to undefined behaviour.\n\
+Please, stop your cluster first." % (clusterName))
+
 try:
     AdminTask.deleteCluster(['-clusterName', clusterName])
 except:
