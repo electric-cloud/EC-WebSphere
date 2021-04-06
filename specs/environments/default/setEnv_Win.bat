c99593952a028a7a945ec1358c7ce5ccb352b44c
Anton Horodchuk <antoman1994@gmail.com>
2021-04-06 20:44:57 +0300

    Switching default environment to 'v90s'

--- specs/environments/default/setEnv_Win.bat
+++ specs/environments/default/setEnv_Win.bat
@@ -1,10 +1,8 @@
 set WAS_USERNAME=wsadmin
 set WAS_PASSWORD=changeme
-set WAS_HOST=websphere90nd
-set WAS_PORT=8879
+set WAS_HOST=websphere90s
+set WAS_PORT=8880
+set WAS_CONNTYPE=SOAP
 set WAS_DEBUG=1
-set WSADMIN_PATH=/opt/IBM/WebSphere/AppServer/profiles/Dmgr01/bin/wsadmin.sh
-set WAS_APPPATH=/var/tmp/
-set WAS_START_DM_SCRIPT=/opt/IBM/WebSphere/AppServer/profiles/Dmgr01/bin/startManager.sh
-set WAS_STOP_DM_SCRIPT=/opt/IBM/WebSphere/AppServer/profiles/Dmgr01/bin/stopManager.sh
-set IS_WAS_ND=1
+set WSADMIN_PATH=/opt/IBM/WebSphere/AppServer/profiles/AppSrv01/bin/wsadmin.sh
+set WAS_APPPATH=C:\
