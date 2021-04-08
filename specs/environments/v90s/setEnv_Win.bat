eccc350b99d9c98bd409bc7afadf7b398e673df5
Anton Horodchuk <antoman1994@gmail.com>
2021-04-08 17:00:36 +0300

    Revert "Switching default environment to 'v90s'"

---
+++ specs/environments/v90s/setEnv_Win.bat
@@ -0,0 +1,8 @@
+set WAS_USERNAME=wsadmin
+set WAS_PASSWORD=changeme
+set WAS_HOST=websphere90s
+set WAS_PORT=8880
+set WAS_CONNTYPE=SOAP
+set WAS_DEBUG=1
+set WSADMIN_PATH=/opt/IBM/WebSphere/AppServer/profiles/AppSrv01/bin/wsadmin.sh
+set WAS_APPPATH=C:\
